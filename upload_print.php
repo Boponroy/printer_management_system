e68dfb3aa96d02a9a23091b206ec2465cee0f832
Bopon Roy <boponroy@gmail.com>
2025-12-11 16:01:40 +0600

    idk man

--- upload_print.php
+++ upload_print.php
@@ -6,6 +6,20 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>upload print</h1>
+    <h2>Upload File for Printing</h2>
+
+    <form action="upload_handler.php" method="POST" enctype="multipart/form-data">
+
+        <label for="printFile">Select File:</label>
+        <input type="file" name="printFile" id="printFile" required>
+        <br><br>
+
+        <label for="pageCount">Number of Pages:</label>
+        <input type="number" name="pageCount" id="pageCount" min="1" required>
+        <br><br>
+
+        <button type="submit" name="uploadSubmit">Submit for Print</button>
+
+    </form>
 </body>
 </html>
